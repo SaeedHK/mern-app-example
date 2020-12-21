55c0d55d0ac4deb1d59f861c00c63be13343b388
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2020-12-21 18:49:25 +0100

    Add middleware to express to use static frontend files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,32 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 
 dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (_, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (_, res) => {
+    res.send("API is running....");
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
